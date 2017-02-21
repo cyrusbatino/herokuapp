1cc9148134c856a706ecc6a41716532b68db3f1d
Cyrus-Jan Batino <cyrusjan.batino@berkeley.edu>
2017-02-20 22:46:21 -0800

    added debug switch

--- cyrusjan/settings.py
+++ cyrusjan/settings.py
@@ -24,7 +24,8 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 SECRET_KEY = "viu_hf1j@9)68cxc$it6d&9yu%$oc#gtd5fd)ocq^5w0kbdk2c"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DJANGO_DEBUG', ''))
+TEMPLATE_DEBUG = DEBUG
 
 # Application definition
 
